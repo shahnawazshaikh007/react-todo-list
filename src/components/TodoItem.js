a2571d401b63aaea34b91159a9271138cd17bc1f
shahnawaz <sk.shahnawaz626265@gmail.com>
2020-08-30 22:49:47 -0700

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is to do item</h1>
+            </div>
+        )
+    }
+}
